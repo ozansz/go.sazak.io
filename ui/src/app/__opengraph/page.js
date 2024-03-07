beea84615e16f6b2cc15891275b6e3d1b4e0dfc2
Ozan Sazak <ozan.sazak@protonmail.ch>
2024-03-07 20:00:38 +0100

    update opengraph and twitter images

--- ui/src/app/__opengraph/page.js
+++ ui/src/app/__opengraph/page.js
@@ -1,14 +1,17 @@
 import { Separator } from "@/components/ui/separator";
+import { cn } from "@/lib/utils";
+
+const debug = false
 
 export default function OpengraphPage() {
     return (
-      <div className="h-[315px] w-[600px] flex flex-row align-center justify-center items-center mx-5">
-          <img src="/assets/pp.jpeg" width={50} height={50} className="rounded-full" />
-          <Separator orientation="vertical" className="mx-5 bg-gray-500 h-12" />
+      <div className={cn("h-[315px] w-[600px] flex flex-row align-center justify-center items-center mx-5", debug ? "border-red-500 border-2" : "")}>
+          <img src="/assets/pp.jpeg" width={100} height={100} className="rounded-full" />
+          <Separator orientation="vertical" className="mx-5 bg-gray-600 h-20" />
           <div>
-            <div className="text-3xl">go.sazak.io</div>
-            <Separator className="my-2 bg-gray-500" />
-            <div className="text-sm text-muted-foreground">Sazak's Go Package Index</div>
+            <div className="text-4xl">go.sazak.io</div>
+            <Separator className="mt-4 mb-2 bg-gray-600" />
+            <div className="text-lg text-muted-foreground">Sazak's Go Package Index</div>
           </div>
       </div>
     )
